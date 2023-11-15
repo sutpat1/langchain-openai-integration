170772d4ad5ab777341faae9759301156d9833fc
Sharv Utpat <103772780+sutpat1@users.noreply.github.com>
2023-11-14 22:38:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from langchain.llms import OpenAI
 import os
 
 print('Hello World')
-OPENAI_API_KEY="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"
+OPENAI_API_KEY="hiddenKey"
+#keycode above is hidden
 os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
 llm = OpenAI(temperature = 0.2)
 response = llm.predict("what is population of united states?")
