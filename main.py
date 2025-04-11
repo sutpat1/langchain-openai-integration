0328503b3761b798057561f4c4dc571cce48a21a
Sharv Utpat <sutpat1@gmail.com>
2025-04-11 17:37:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 print('Hello World')
 OPENAI_API_KEY="hiddenKey"
-#keycode above is hidden
+#replace hiddenKey with your own OpenAI API key
 os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
 llm = OpenAI(temperature = 0.2)
 response = llm.predict("what is population of united states?")
